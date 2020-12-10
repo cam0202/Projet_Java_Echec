b16323c272c8bb23329ea541fc66b7334d7f88e7
cam0202 <camilleclaret@orange.fr>
2020-12-10 10:39:38 +0100

    Class de la reine

--- server/src/main/java/game/Queen.java
+++ server/src/main/java/game/Queen.java
@@ -1,5 +1,37 @@
 package game;
 
 public class Queen {
-
+	private Chessman cm;
+	private String c;
+	
+	/**
+	 * Constructeur
+	 * @param chessman
+	 */
+	Queen(Chessman chessman){
+		this.cm = chessman;
+		this.c = this.cm.getColor();
+	}
+	
+	/**
+	 * Fonctions qui retourne les infos
+	 */
+	String getType() {
+		return this.cm.getName();
+	}
+	
+	String getColor() {
+		return this.c;
+	}
+	
+	/**
+	 *  Déplacement de la reine 
+	 * @param move
+	 * @return
+	 */
+	boolean moveQueen(Move move){
+		// déplacement en diagonale ou rectiligen 
+		return (Math.abs(move.getLocationX()) - Math.abs(move.getLocationY()) == 0 
+				|| move.getLocationX() * move.getLocationY() == 0) && !move.isNul();
+	}
 }
