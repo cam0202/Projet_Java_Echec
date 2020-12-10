675b44e4958fc51c62736c2e0705cad52d4ba416
cam0202 <camilleclaret@orange.fr>
2020-12-10 10:48:40 +0100

    Class de la tour

--- server/src/main/java/game/Rook.java
+++ server/src/main/java/game/Rook.java
@@ -1,6 +1,38 @@
 package game;
 
 public class Rook {
-	// tour
+	
+	private Chessman cm;
+	private String c;
+	
+	/**
+	 * Constructeur
+	 * @param chessman
+	 */
+	Rook(Chessman chessman){
+		this.cm = chessman;
+		this.c = this.cm.getColor();
+	}
+	
+	/**
+	 * Fonctions qui retourne les infos
+	 */
+	String getType() {
+		return this.cm.getName();
+	}
+	
+	String getColor() {
+		return this.c;
+	}
+	
+	/**
+	 *  Déplacement de la reine 
+	 * @param move
+	 * @return
+	 */
+	boolean moveRook(Move move){
+		// déplacement en vertical 
+		return move.getLocationX() * move.getLocationY() == 0 && !move.isNul();
+	}
 
 }
