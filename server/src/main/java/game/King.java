18aa34d079fc41a268e85b04eda1afb3247fd9f2
cam0202 <camilleclaret@orange.fr>
2020-12-10 10:36:44 +0100

    Class du roi

--- server/src/main/java/game/King.java
+++ server/src/main/java/game/King.java
@@ -1,5 +1,40 @@
 package game;
 
 public class King {
+	private Chessman cm;
+	private String c;
+	
+	/**
+	 * Constructeur
+	 * @param chessman
+	 */
+	King(Chessman chessman){
+		this.cm = chessman;
+		this.c = this.cm.getColor();
+	}
+	
+	/**
+	 * Fonctions qui retourne les infos
+	 */
+	String getType() {
+		return this.cm.getName();
+	}
+	
+	String getColor() {
+		return this.c;
+	}
+	
+	/**
+	 *  Déplacement du roi
+	 * @param move
+	 * @return
+	 */
+	boolean moveKing(Move move){
+		// le roi ne peux se déplacer que 1 case 
+		return Math.abs(move.getLocationX()) * Math.abs(move.getLocationY()) <= 1  
+				&& Math.abs(move.getLocationX()) - Math.abs(move.getLocationY()) <= 1
+				&& Math.abs(move.getLocationX()) - Math.abs(move.getLocationY()) >= -1
+				&& !move.isNul();
+	}
 
 }
