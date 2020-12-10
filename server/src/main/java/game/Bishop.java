dd0ef34a5004d1a0a05ed5884942d7ad34258e46
cam0202 <camilleclaret@orange.fr>
2020-12-10 10:46:28 +0100

    Class du fous

--- server/src/main/java/game/Bishop.java
+++ server/src/main/java/game/Bishop.java
@@ -1,5 +1,36 @@
 package game;
 
 public class Bishop {
-	// Fous
+	private Chessman cm;
+	private String c;
+	
+	/**
+	 * Constructeur
+	 * @param chessman
+	 */
+	Bishop(Chessman chessman){
+		this.cm = chessman;
+		this.c = this.cm.getColor();
+	}
+	
+	/**
+	 * Fonctions qui retourne les infos
+	 */
+	String getType() {
+		return this.cm.getName();
+	}
+	
+	String getColor() {
+		return this.c;
+	}
+	
+	/**
+	 *  Déplacement de la reine 
+	 * @param move
+	 * @return
+	 */
+	boolean moveBishop(Move move){
+		// déplacement en diagonale 
+		return Math.abs(move.getLocationX()) - Math.abs(move.getLocationY()) == 0 && !move.isNul();
+	}
 }
