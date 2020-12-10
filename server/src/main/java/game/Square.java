094ce64f7a663a14511221756bd769168e94d5f4
cam0202 <camilleclaret@orange.fr>
2020-12-10 17:44:45 +0100

    Square class + tests

--- server/src/main/java/game/Square.java
+++ server/src/main/java/game/Square.java
@@ -1,5 +1,50 @@
 package game;
 
 public class Square {
-
+	/**
+	 * Piece contenu par la case
+	 */
+	private Chessman cm;
+	
+	/**
+	 * Constructeurs
+	 */
+	public Square()
+	{
+		this.cm = null;
+	}
+	
+	public Square(Chessman chessman)
+	{
+		this.setChessman(chessman);
+	}
+	
+	
+	public Chessman getChessman () {
+		return this.cm;
+	}
+	
+	/**
+	 * Methode met un objet Piece sur la case
+	 * @param Piece � placer
+	 */
+	public void setChessman(Chessman chessman) {
+		this.cm = chessman;
+	}
+	
+	/**
+	 * Methode estOccupe servant a savoir si la case est occupe ou non.
+	 */
+	public boolean isTaken()
+	{
+		return (this.cm != null);	
+	}
+	
+	public boolean isTaken(String couleur)
+	{
+		if (this.cm == null)
+			return false;
+		else
+			return (this.cm.getColor().equals(couleur));
+	}
 }
