1b4ff88bf61bb49383c9580ebac0ffdb8227408e
cam0202 <camilleclaret@orange.fr>
2020-12-10 16:40:06 +0100

    Test Move and Location

---
+++ server/src/test/java/game/MoveTest.java
@@ -0,0 +1,48 @@
+package game;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class MoveTest {
+	
+	Move m;
+	Location start;
+	Location end;
+
+	@Before
+	public void setUp() throws Exception {
+		this.start = new Location(1,1);
+		this.end = new Location(1,1);
+		this.m = new Move(start, end);
+	}
+
+	@Test
+	public void testGetLocationX() {
+		assertEquals ((int)this.m.getLocationX(), 0);
+	}
+	
+	@Test
+	public void testGetLocationY() {
+		assertEquals ((int)this.m.getLocationY(), 0);
+	}
+	
+	@Test
+	public void testGetStart() {
+		assertEquals (this.m.getStart(), this.start);
+	}
+	
+	@Test
+	public void testGetEnd() {
+		assertEquals (this.m.getEnd(), this.end);
+	}
+	
+	@Test
+	public void testIsNul() {
+		assertEquals (this.m.isNul(), true);
+		Move m2 = new Move(this.start, new Location(1,3));
+		assertEquals (m2.isNul(), false);
+	}
+
+}
