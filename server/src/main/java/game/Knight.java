43084418ba36c00db3fd2f78aec38781c68faf75
cam0202 <camilleclaret@orange.fr>
2020-12-10 10:44:36 +0100

    Class du cavalier

--- server/src/main/java/game/Knight.java
+++ server/src/main/java/game/Knight.java
@@ -1,6 +1,38 @@
 package game;
 
 public class Knight {
-	// cavalier
+	private Chessman cm;
+	private String c;
+	
+	/**
+	 * Constructeur
+	 * @param chessman
+	 */
+	Knight(Chessman chessman){
+		this.cm = chessman;
+		this.c = this.cm.getColor();
+	}
+	
+	/**
+	 * Fonctions qui retourne les infos
+	 */
+	String getType() {
+		return this.cm.getName();
+	}
+	
+	String getColor() {
+		return this.c;
+	}
+	
+	/**
+	 *  Déplacement de la reine 
+	 * @param move
+	 * @return
+	 */
+	boolean moveKnight(Move move){
+		// movement en L 
+		return (Math.abs(move.getLocationX()) / move.getLocationY()) == 2   
+				|| (Math.abs(move.getLocationX() / move.getLocationY()) == .5);
+	}
 
 }
