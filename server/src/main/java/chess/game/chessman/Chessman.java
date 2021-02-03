e0af36491ec48cd8d23f9059c9d5e01c6da545a4
cam0202 <camilleclaret@orange.fr>
2021-02-03 09:49:47 +0100

    MaJ class Chessman pour assigner des attaques à chaque pièce

--- server/src/main/java/chess/game/chessman/Chessman.java
+++ server/src/main/java/chess/game/chessman/Chessman.java
@@ -7,10 +7,14 @@ public abstract class Chessman {
 
     private final String name;
     private final Player player;
+    private int live;
+    private Attack[] attacks;
 
-    public Chessman(String name, Player player) {
+    public Chessman(String name, Player player, int live, Attack[] attacks) {
         this.name = name;
         this.player = player;
+        this.live = live;
+        this.attacks = attacks;
     }
 
     public String getName() {
@@ -19,6 +23,18 @@ public abstract class Chessman {
 
     public Player getPlayer() {
         return this.player;
+    }
+
+    public int getLive() {
+        return this.live;
+    }
+
+    public Attack[] getAttacks() {
+        return attacks;
+    }
+
+    public void setLive(int impact) {
+        this.live = this.live - impact;
     }
 
     public abstract boolean canMove(Move move);
